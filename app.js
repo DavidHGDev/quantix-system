ed03b1726c9357ec39faa281a6e94886122c8eee
David H <davyd2h@gmail.com>
2026-05-19 14:29:39 -0500

    nuevo entorno con pnpm

--- app.js
+++ app.js
@@ -2,14 +2,23 @@ import e from "express";
 import mainRouter from './routes/index.js'
 import { manejadorDeErrores } from "./middlewares/error.Handler.js";
 import cors from 'cors';
+import path from 'path';
+import { fileURLToPath } from "url";
+
 
 // const corsOption = {
 //     origin: 'http://localhost:5500/',
 //     optionsSuccesStatus: 200
 // }
 
+const __dirfile = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__dirfile);
+const ruta = path.join(__dirname, 'public'); // ruta lista absoluna 
+
 const app = e();
 const PORT = 3007;
+
+app.use(e.static(ruta)); //Servimos la ruta public
 
 app.use(e.json());
 app.use(cors())
